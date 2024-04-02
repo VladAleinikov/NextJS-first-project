03bbaf40bb9ddb7c32c29b52a94f4f06f764bbde
Vladik <55589606+VladAleinikov@users.noreply.github.com>
2024-04-02 10:29:09 +0300

    create client side next app

---
+++ client/app/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar"
+
+const Index = () => {
+  return (
+    <div>
+      <Navbar/>
+      <h1>Добро пожаловать</h1>
+      <h3>Здесь собраны лучшие треки!</h3>
+    </div>
+  )
+}
+
+export default Index
