dd4ab119e7f0adf1a95d448e87353c55b6b7ba21
Vladik <55589606+VladAleinikov@users.noreply.github.com>
2024-04-03 12:18:23 +0300

    add track creation

--- client/app/tracks/create/page.tsx
+++ client/app/tracks/create/page.tsx
@@ -1,15 +1,30 @@
 "use client";
 import FileUpload from "@/components/FileUpload";
 import StepWrapper from "@/components/StepWrapper";
-import React, { useState } from "react";
+import { useLazyAddTrackQuery } from "@/lib/track/track.api";
+import { useRouter } from "next/navigation";
+import React, { useRef, useState } from "react";
 
 const Create = () => {
+  const router = useRouter();
   const [activeStep, setActiveStep] = useState<number>(0);
-  const [picture, setPicture] = useState(null)
-  const [audio, setAudio] = useState(null)
+  const [picture, setPicture] = useState(null);
+  const [audio, setAudio] = useState(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const artistRef = useRef<HTMLInputElement>(null);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+  const [addTrack, {isLoading, isError}] = useLazyAddTrackQuery();
   const next = () => {
     if (activeStep !== 2) {
       setActiveStep((prev) => prev + 1);
+    } else if(picture && audio) {
+      const formData = new FormData();
+      formData.append("name", nameRef.current?.value || '');
+      formData.append("artist", artistRef.current?.value || '');
+      formData.append("text", textRef.current?.value || '');
+      formData.append("picture", picture);
+      formData.append("audio", audio);
+      addTrack(formData).then(res => router.push('/tracks'));
     }
   };
   const back = () => {
@@ -20,13 +35,26 @@ const Create = () => {
       <StepWrapper activeStep={activeStep}>asdasd</StepWrapper>
       {activeStep === 0 && (
         <div>
-          <input type="text" placeholder="Название трека" />
-          <input type="text" placeholder="Имя автора" />
-          <textarea cols="30" rows="10" placeholder="Слова к треку"></textarea>
+          <input ref={nameRef} type="text" placeholder="Название трека" />
+          <input ref={artistRef} type="text" placeholder="Имя автора" />
+          <textarea
+            ref={textRef}
+            cols={30}
+            rows={10}
+            placeholder="Слова к треку"
+          ></textarea>
         </div>
       )}
-      {activeStep === 1 && <FileUpload setFile={setPicture} accept="image/*" ><button>Загрузить изображение</button></FileUpload>}
-      {activeStep === 2 && <FileUpload setFile={setAudio} accept="audio/*" ><button>Загрузить аудио</button></FileUpload>}
+      {activeStep === 1 && (
+        <FileUpload setFile={setPicture} accept="image/*">
+          <button>Загрузить изображение</button>
+        </FileUpload>
+      )}
+      {activeStep === 2 && (
+        <FileUpload setFile={setAudio} accept="audio/*">
+          <button>Загрузить аудио</button>
+        </FileUpload>
+      )}
       <div>
         <button disabled={activeStep === 0} onClick={back}>
           Назад
--- client/lib/track/track.api.ts
+++ client/lib/track/track.api.ts
@@ -2,16 +2,22 @@ import { ITrack } from "@/types/track";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const tracksApi = createApi({
-      reducerPath: 'tracksApi',
-      baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
-      endpoints: (builder) => ({
-            fetchTracks: builder.query<ITrack[], void>({
-                  query: () => ({
-                        url: 'tracks',
-                  })
-            })
-      })
-}
-)
+  reducerPath: "tracksApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  endpoints: (builder) => ({
+    fetchTracks: builder.query<ITrack[], void>({
+      query: () => ({
+        url: "tracks",
+        method: "get",
+      }),
+    }),
+    addTrack: builder.query<ITrack, FormData>({
+      query: () => ({
+        url: "tracks",
+        method: "post",
+      }),
+    }),
+  }),
+});
 
-export const { useFetchTracksQuery} = tracksApi;
+export const { useFetchTracksQuery, useLazyAddTrackQuery } = tracksApi;
